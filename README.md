d1db110631945950a2cc17862e02006b4b4b6829
yanxxit <yanxxit@163.com>
2017-08-29 10:37:03 +0800

    httputil 功能优化

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # beego-common-utils
 beego 通用工具
+
+
+## 安装
+
+```text
+go get -u github.com/shawflying/beego-common-utils
+```
--- utils/httputil/httputil.go
+++ utils/httputil/httputil.go
@@ -5,21 +5,23 @@ import (
 	"time"
 	"beego-common-utils/utils/comutil"
 	"beego-common-utils/utils/logger"
-	"strconv"
-	"github.com/astaxie/beego"
 	"encoding/json"
 )
 
 //返回的结果 为[]byte  error
 //入参 reqUrl string + map 或者结构体
-var httpCT time.Duration
-var httpRWT time.Duration
+var HttpCT time.Duration
+var HttpRWT time.Duration
+
+var Cookie string //cookie
+
+var Header map[string](string) //header
+
+var Token string//token
 
 func init() {
-	httpConnectTimeout, _ := strconv.Atoi(beego.AppConfig.String("httpConnectTimeout")) //time.Duration
-	httpReadWriteTimeout, _ := strconv.Atoi(beego.AppConfig.String("httpReadWriteTimeout"))
-	httpCT = time.Duration(httpConnectTimeout) * time.Second
-	httpRWT = time.Duration(httpReadWriteTimeout) * time.Second
+	HttpCT = 5 * time.Second
+	HttpRWT = 5 * time.Second
 }
 
 func getResponseTime(start time.Time) time.Duration {
@@ -30,18 +32,18 @@ func getResponseTime(start time.Time) time.Duration {
 func Get(reqUrl string) (content []byte, err error) {
 	start := time.Now()
 	logger.Info("request-get-url: " + reqUrl)
-	req := httplib.Get(reqUrl).SetTimeout(httpCT, httpRWT)
+	req := httplib.Get(reqUrl).SetTimeout(HttpCT, HttpRWT)
 	data, err := req.Bytes()
 	logger.Info("request-get-data: "+string(data), err)
 	logger.Info("request-get-response-time:", getResponseTime(start))
 	return data, err
 }
 
-func Post(reqUrl string, params interface{}, token string) (content []byte, err error) {
+func Post(reqUrl string, params interface{}) (content []byte, err error) {
 	start := time.Now()
 	logger.Info("request-post-url: " + reqUrl)
 	logger.Info("request-post-params: ", params)
-	req, err := httplib.Post(reqUrl).Header("Content-Type", "application/json").Header("token", token).SetTimeout(httpCT, httpRWT).JSONBody(params)
+	req, err := httplib.Post(reqUrl).Header("Content-Type", "application/json").SetTimeout(HttpCT, HttpRWT).JSONBody(params)
 	data, err := req.Bytes()
 	logger.Info("request-post-data: "+string(data), err)
 	logger.Info("request-post-response-time:", getResponseTime(start))
@@ -60,7 +62,7 @@ func PostForm(reqUrl string, params interface{}) (content []byte, err error) {
 	mapParams := make(map[string]interface{});
 	json.Unmarshal(byts, &mapParams)
 
-	req := httplib.Post(reqUrl).SetTimeout(httpCT, httpRWT)
+	req := httplib.Post(reqUrl).SetTimeout(HttpCT, HttpRWT)
 	for k, v := range mapParams {
 		req.Param(k, comutil.TransInterfaceToString(v))
 	}
@@ -74,7 +76,7 @@ func Put(reqUrl string, params interface{}) (content []byte, err error) {
 	logger.Info("request-put-url: " + reqUrl)
 	logger.Info("request-put-params: ", params)
 	start := time.Now()
-	req, err := httplib.Put(reqUrl).SetTimeout(httpCT, httpRWT).JSONBody(params)
+	req, err := httplib.Put(reqUrl).SetTimeout(HttpCT, HttpRWT).JSONBody(params)
 	data, err := req.Bytes()
 	strs, _ := req.String()
 	logger.Info("request-put-string: ", strs, err)
