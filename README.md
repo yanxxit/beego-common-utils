c7cadeb0ca1733bbc6b1deec494ce6c72be359af
yanxxit <yanxxit@163.com>
2017-08-29 13:27:48 +0800

    common 响应优化

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ beego 通用工具
 ```text
 go get -u github.com/shawflying/beego-common-utils
 ```
+
+
+## 测试
+
+header 1 | header 2
+---|---
+row 1 col 1 | row 1 col 2
+row 2 col 1 | row 2 col 2
--- common/common.go
+++ common/common.go
@@ -36,10 +36,25 @@ func (this *CommonController) ResJson(data interface{}) {
 	this.StopRun()
 }
 
+func (this *CommonController) ResResult(status int, info string, data interface{}) {
+	logger.Info(" ResData:", comutil.InterfaceToJSON(data))
+	var result map[string]interface{} = make(map[string]interface{})
+	result["status"] = status
+	result["info"] = info
+	result["data"] = data
+	this.Data["json"] = result
+	this.ServeJSON()
+	this.StopRun()
+}
+
 //返回通用成功数据
 func (this *CommonController) ResSuccess(data interface{}) {
 	logger.Info(" ResData:", comutil.InterfaceToJSON(data))
-	this.Data["json"] = data
+	var success map[string]interface{} = make(map[string]interface{})
+	success["status"] = 200
+	success["info"] = ""
+	success["data"] = data
+	this.Data["json"] = success
 	this.ServeJSON()
 	this.StopRun()
 }
