817488a2ce58c4be52200b58914c7f4a16602f1e
yanxxit <yanxxit@163.com>
2017-12-12 15:48:11 +0800

    <添加body转换为map><update>

--- common/common.go
+++ common/common.go
@@ -5,6 +5,7 @@ import (
 	"github.com/shawflying/beego-common-utils/utils/logger"
 	"github.com/shawflying/beego-common-utils/utils/comutil"
 	"github.com/shawflying/beego-common-utils/open"
+	"encoding/json"
 )
 
 type CommonController struct {
@@ -29,4 +30,15 @@ func (this *CommonController) ResJson(data interface{}) {
 	this.Data["json"] = comutil.InterfaceTo2(data)
 	this.ServeJSON()
 	this.StopRun()
+}
+
+//将post 请求的body 转换为map
+func (c *CommonController) RequestBodyToStringMap() (body map[string]string, err error) {
+	err = json.Unmarshal(c.Ctx.Input.RequestBody, &body)
+	return body, err
+}
+
+func (c *CommonController) RequestBodyToInterfaceMap() (body map[string]interface{}, err error) {
+	err = json.Unmarshal(c.Ctx.Input.RequestBody, &body)
+	return body, err
 }
